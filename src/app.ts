a248a9b55138f816ef4de9cad14ef3b109f1ff59
Jesuloba <jesulobajohn@gmail.com>
2021-05-03 20:14:57 +0100

    Type guard

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,55 @@ const e1: ElevatedEmployee = {
 	startDate: new Date(),
 };
 
-type Combinable = number | string;
-type Numeric = number | boolean;
+// type Combinable = number | string;
+// type Numeric = number | boolean;
 
-type Universal = Combinable & Numeric;
+// type Universal = Combinable & Numeric;
+
+type unknownEmployee = Admin | Employee;
+
+// You can also use the 'typeof' operator
+// but it only works for javascript built in types
+
+// Type guard using 'in' operator
+// Works for both classes and types
+function printEmployeeInformation(emp: unknownEmployee) {
+	console.log(`Name: ${emp.name}`);
+	if ("privileges" in emp) console.log(`Privileges: ${emp.privileges}`);
+	if ("startDate" in emp) console.log(`startDate: ${emp.startDate}`);
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+	drive() {
+		console.log("Driving...");
+	}
+}
+
+class Truck {
+	drive() {
+		console.log("Driving a Truck...");
+	}
+
+	loadCargo(amount: number) {
+		console.log("Loading cargo... " + amount);
+	}
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+// Type guard using 'instanceof'
+// Only works for classes
+function useVehicle(vehicle: Vehicle) {
+	vehicle.drive();
+	if (vehicle instanceof Truck) {
+		vehicle.loadCargo(1000);
+	}
+}
+
+useVehicle(v1);
+useVehicle(v2);

3ff24fc4bb4f623d0dc2db15e8d85741b82a9d13
Jesuloba <jesulobajohn@gmail.com>
2021-05-08 19:22:11 +0100

    useful decorator

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,19 @@ function Logger(logString: string) {
 	};
 }
 
-@Logger("LOGGING - PERSON")
+function WithTemplate(template: string, hookId: string) {
+	return function (constructor: any) {
+		const hookEl = document.getElementById(hookId);
+		const p = new constructor();
+		if (hookEl) {
+			hookEl.innerHTML = template;
+			document.querySelector("h1")!.textContent = `my name is ${p.name}`;
+		}
+	};
+}
+
+// @Logger("LOGGING - PERSON")
+@WithTemplate("<h3>My person object</h3>", "app")
 class Person {
 	name = "John";
 

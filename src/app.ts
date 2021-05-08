868352b6f717b28c0b6cf43a89972357b89de300
Jesuloba <jesulobajohn@gmail.com>
2021-05-08 03:06:29 +0100

    generic classes

--- src/app.ts
+++ src/app.ts
@@ -58,3 +58,30 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 extractAndConvert({ name: "Max" }, "name");
+
+/**
+ *  Generic classes
+ */
+
+class DataStorage<T extends string | number | boolean> {
+	private data: T[] = [];
+
+	addItem(item: T) {
+		this.data.push(item);
+	}
+
+	removeItem(item: T) {
+		this.data.splice(this.data.indexOf(item), 1);
+	}
+
+	getItems() {
+		return [...this.data];
+	}
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("John");
+textStorage.addItem("Max");
+textStorage.addItem("Manu");
+textStorage.removeItem("Manu");
+console.log(textStorage.getItems());

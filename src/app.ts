605003c9a8834a11eb9699b9ef114b095410d2a4
Jesuloba <jesulobajohn@gmail.com>
2021-05-08 19:27:28 +0100

    adding multiple decorators to a class

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
 	return function (constructor: any) {
+		console.log("rendering template");
 		const hookEl = document.getElementById(hookId);
 		const p = new constructor();
 		if (hookEl) {
@@ -20,7 +21,7 @@ function WithTemplate(template: string, hookId: string) {
 	};
 }
 
-// @Logger("LOGGING - PERSON")
+@Logger("LOGGING")
 @WithTemplate("<h3>My person object</h3>", "app")
 class Person {
 	name = "John";

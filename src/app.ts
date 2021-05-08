a6eb301be8b65b28e04719a25b037eb84b3bc110
Jesuloba <jesulobajohn@gmail.com>
2021-05-08 02:36:30 +0100

    generic function

--- src/app.ts
+++ src/app.ts
@@ -17,9 +17,31 @@
  * Custom generics
  */
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
 	return Object.assign(objA, objB);
 }
 
 const mergedObject = merge({ name: "Max" }, { age: 20 });
 console.log(mergedObject.name);
+
+/**
+ *  Generic Function
+ */
+
+// only properties with the length value will be allowed into the function
+interface Lenghty {
+	length: number;
+}
+
+function countAndDescribe<T extends Lenghty>(element: T): [T, string] {
+	let descriptionText = "Got no value.";
+	if (element.length === 1) {
+		descriptionText = "Got 1 element.";
+	} else if (element.length > 1) {
+		descriptionText = `Got ${element.length} elements`;
+	}
+
+	return [element, descriptionText];
+}
+
+console.log(countAndDescribe("hi there!"));

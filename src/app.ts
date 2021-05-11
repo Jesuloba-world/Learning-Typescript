5ba188a93c9c1167a4f21e3b1ec62f2f5eae219e
Jesuloba <jesulobajohn@gmail.com>
2021-05-11 18:55:41 +0100

    property decorators

--- src/app.ts
+++ src/app.ts
@@ -34,3 +34,33 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+// property decorators
+
+function Log(target: any, propertyName: string) {
+	console.log("property decorator!");
+	console.log(target, propertyName);
+}
+
+class product {
+	@Log
+	title: string;
+	private _price: number;
+
+	set price(val: number) {
+		if (val > 0) {
+			this._price = val;
+		} else {
+			throw new Error("Invalid price - should be positive");
+		}
+	}
+
+	constructor(t: string, p: number) {
+		this.title = t;
+		this._price = p;
+	}
+
+	getPriceWithTax(tax: number) {
+		return this._price * (1 + tax);
+	}
+}

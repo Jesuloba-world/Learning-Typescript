f25d826485549de45e386e1d1160fd7892790956
Jesuloba <jesulobajohn@gmail.com>
2021-05-12 08:38:56 +0100

    Autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -101,3 +101,33 @@ class product {
 		return this._price * (1 + tax);
 	}
 }
+
+//----------------------------------------------------------------
+// Autobind decorator
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+	const originalMethod = descriptor.value;
+	const adjDescriptor: PropertyDescriptor = {
+		configurable: true,
+		enumerable: false,
+		get() {
+			const boundFn = originalMethod.bind(this);
+			return boundFn;
+		},
+	};
+	return adjDescriptor;
+}
+
+class Printer {
+	message: string = "This works!";
+
+	@Autobind
+	showMessage() {
+		console.log(this.message);
+	}
+}
+
+const p = new Printer();
+
+const button = document.querySelector("button")!;
+button.addEventListener("click", p.showMessage);
